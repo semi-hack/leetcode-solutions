58fc540745b7a88f8adf7b438fbc2034c1d5557d
Akinlo Oluwasemiloore <asemiloore@gmail.com>
2022-11-03 09:08:26 +0100

    Create intToRoman.ts

---
+++ intToRoman.ts
@@ -0,0 +1,8 @@
+function intToRoman(num: number): string {
+    const value = [1000,900, 500, 400, 100, 90, 50, 40, 10, 9,  5, 4, 1]
+    const roman = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    let ans = ""
+    for (let i = 0; num; i++)
+        while (num >= value[i]) ans += roman[i], num -= value[i]
+    return ans
+};
